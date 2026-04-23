871d03de2de9e550f543c9b99544ceecc9f74cd8
Sevinch <sevinchqadamboyeva0@gmail.com>
2026-04-23 16:02:55 +0500

    all codes

--- app.js
+++ app.js
@@ -2,30 +2,55 @@
 const button = document.querySelector(".btn");
 const first_player_img = document.querySelector(".first_player");
 const second_player_img = document.querySelector(".second_player");
-const btn = document.querySelector(".result");
+const result = document.querySelector(".result");
+
+result.textContent = "you are first player. Click the button to start a game!"
 
 button.addEventListener("click", () => {
-    const first_random_number = Math.round(Math.random() * 2);
-      const second_random_number = Math.round(Math.random() * 2);
-    })
+  const first_random_number = Math.round(Math.random() * 2);
+  const second_random_number = Math.round(Math.random() * 2);
+
+  if (first_random_number == 0) {
+    first_player_img.setAttribute('src', './img/paper.jpg')
+  } else if (first_random_number == 1) {
+    first_player_img.setAttribute('src', './img/rock.jpg')
+  } else {
+    first_player_img.setAttribute('src', './img/scissors.jpg')
+  }
 
 
-    if(first_random_number == 0) {
-      first_player_img.setAttribute('src', './img/paper.png')
-    }else if(first_random_number == 1) {
-      first_player_img.setAttribute('src', './img/rock.png')
-    }else{
-       first_player_img.setAttribute('src', './img/scissors.png')
-    }
+  if (second_random_number == 0) {
+    second_player_img.setAttribute('src', './img/paper.jpg')
+  } else if (second_random_number == 1) {
+    second_player_img.setAttribute('src', './img/rock.jpg')
+  } else {
+    second_player_img.setAttribute('src', './img/scissors.jpg')
+  }
+
+  if(first_random_number == 0 && second_random_number == 1) {
+    result.textContent = "you won!";
+    result.style.color = "green";
+  } else if (first_random_number == 1 && second_random_number == 2) {
+    result.textContent = "you won!";
+    result.style.color = "green";
+  } else if (first_random_number == 2 && second_random_number == 0) {
+    result.textContent = "you won!";
+    result.style.color = "green";
+  }
+
+  else if (first_random_number == second_random_number) {
+     result.textContent = "draw";
+     result.style.color = "blue";
+  }
+
+else {
+  result.textContent = "you lost";
+  result.style.color = "red";
+}
 
 
-    if(second_random_number == 0) {
-      second_player_img.setAttribute('src', './img/paper.png')
-    }else if(second_random_number == 1) {
-      second_player_img.setAttribute('src', './img/rock.png')
-    }else{
-       second_player_img.setAttribute('src', './img/scissors.png')
-    }
+})
+
 
 
 
