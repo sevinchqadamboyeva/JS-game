1a54e5f56d94a6c97652fd9942e15ee29fa6b938
Sevinch <sevinchqadamboyeva0@gmail.com>
2026-04-23 16:21:47 +0500

    codes

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const result = document.querySelector(".result");
 
 result.textContent = "you are first player. Click the button to start a game!"
 
+function getRandomNumber(num) {
+  return Math.round(Math.random() * num);
+}
+
 button.addEventListener("click", () => {
-  const first_random_number = Math.round(Math.random() * 2);
-  const second_random_number = Math.round(Math.random() * 2);
+  const first_random_number = getRandomNumber(2);
+  const second_random_number = getRandomNumber(2);
 
   if (first_random_number == 0) {
     first_player_img.setAttribute('src', './img/paper.jpg')
