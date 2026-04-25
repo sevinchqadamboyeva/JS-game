291667ccc2c2ab4761767c8091cc273c7d65da63
Sevinch <sevinchqadamboyeva0@gmail.com>
2026-04-25 15:49:23 +0500

    added codes

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
-
-const button = document.querySelector(".btn");
+// Kerakli html tag lariga ulanish
 const first_player_img = document.querySelector(".first_player");
 const second_player_img = document.querySelector(".second_player");
 const result = document.querySelector(".result");
+const btn = document.querySelector(".btn");
 
-result.textContent = "you are first player. Click the button to start a game!"
+result.textContent = "You are first player. Click the button to start a game!";
 
+// Takroriy kodlarni kamaytirish uchun funksiya
 function getRandomNumber(num) {
   return Math.round(Math.random() * num);
 }
 
-button.addEventListener("click", () => {
+function setImageAttribute(random_num, img) {
+  if (random_num == 0) {
+    img.setAttribute("src", "./img/paper.jpg");
+  } else if (random_num == 1) {
+    img.setAttribute("src", "./img/rock.jpg");
+  } else {
+    img.setAttribute("src", "./img/scissors.jpg");
+  }
+}
+
+// btn elementiga hodisa qo'shish
+btn.addEventListener("click", () => {
+  // tasodifiy sonlar generatsiya qilish
   const first_random_number = getRandomNumber(2);
   const second_random_number = getRandomNumber(2);
 
-  if (first_random_number == 0) {
-    first_player_img.setAttribute('src', './img/paper.jpg')
-  } else if (first_random_number == 1) {
-    first_player_img.setAttribute('src', './img/rock.jpg')
-  } else {
-    first_player_img.setAttribute('src', './img/scissors.jpg')
-  }
+  //   birinchi o'yinchi uchun tasodifiy rasmlar
+  setImageAttribute(first_random_number, first_player_img);
+  // ikkinchi o'yinchi uchun tasodifiy rasmlar
+  setImageAttribute(second_random_number, second_player_img);
 
-
-  if (second_random_number == 0) {
-    second_player_img.setAttribute('src', './img/paper.jpg')
-  } else if (second_random_number == 1) {
-    second_player_img.setAttribute('src', './img/rock.jpg')
-  } else {
-    second_player_img.setAttribute('src', './img/scissors.jpg')
-  }
-
-  if(first_random_number == 0 && second_random_number == 1) {
-    result.textContent = "you won!";
-    result.style.color = "green";
-  } else if (first_random_number == 1 && second_random_number == 2) {
-    result.textContent = "you won!";
-    result.style.color = "green";
-  } else if (first_random_number == 2 && second_random_number == 0) {
-    result.textContent = "you won!";
+  // birinchi o'yinchi yutadigan kombinatsiyalar
+  if (
+    (first_random_number == 0 && second_random_number == 1) ||
+    (first_random_number == 1 && second_random_number == 2) ||
+    (first_random_number == 2 && second_random_number == 0)
+  ) {
+    result.textContent = "Siz yutdingiz!";
     result.style.color = "green";
   }
-
+  //   Durang bo'ladigan kombinatsiyalar
   else if (first_random_number == second_random_number) {
-     result.textContent = "draw";
-     result.style.color = "blue";
+    result.textContent = "Durang";
+    result.style.color = "blue";
   }
-
-else {
-  result.textContent = "you lost";
-  result.style.color = "red";
-}
-
-
-})
-
-
-
+  //   Ikkinchi o'yinchi yutadigan kombinatsiyalar
+  else {
+    result.textContent = "Siz yutqazdingiz!";
+    result.style.color = "red";
+  }
+});
 
 
 
